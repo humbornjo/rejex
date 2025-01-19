c21519688580c156f764d72ae0aa77a1fe80a9d3
humbornjo <humbornjo@gmail.com>
2025-01-19 17:07:13 +0800

    update: horizontial resizer

--- src/components/Rejexer/Rejexer.tsx
+++ src/components/Rejexer/Rejexer.tsx
@@ -1,17 +1,39 @@
+import { useEffect, useState, useRef } from "react";
+
 function Rejexer(): JSX.Element {
+  const isResizing = useRef(false);
+  const [width, setWidth] = useState(300);
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      if (!isResizing.current) return;
+      console.log(e.movementX);
+      setWidth((prev: number) => prev + e.movementX / 2);
+    });
+    window.addEventListener("mouseup", () => {
+      isResizing.current = false;
+    });
+  }, []);
   return (
-    <div
-      className="w-full h-[calc(100%-48px)]
-      grid grid-cols-2"
-    >
-      <div className="bg-red-500">Input</div>
-      <div className="grid-rows-subgrid row-span-2 bg-green-500 flex">
-        <div className="w-1 bg-slate-500"></div>
-        <div className="">Rejexer</div>
+    <div className="w-full h-[calc(100%-48px)] flex">
+      <div
+        style={{ width: `${width / 16}rem` }}
+        className="h-full flex flex-col"
+      >
+        <div className="bg-red-500">Input</div>
+        <div className="bg-blue-500 flex flex-col grow">
+          <div className="h-1 bg-slate-500"></div>
+          <div className="grow"> Outcome</div>
+        </div>
       </div>
-      <div className="bg-blue-500 flex flex-col">
-        <div className="h-1 bg-slate-500"></div>
-        <div className="grow"> Outcome</div>
+      <div className="bg-green-500 flex grow">
+        <div
+          className="w-1 bg-slate-500 hover:cursor-ew-resize"
+          onMouseDown={(e) => {
+            isResizing.current = true;
+            e.preventDefault();
+          }}
+        ></div>
+        <div className="">Rejexer</div>
       </div>
     </div>
   );
